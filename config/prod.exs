f3763f4ba7be2226f71fa81c09f9cb87f1fbe7bc
Souvik Banerjee <souvik1997@gmail.com>
2016-10-22 15:15:45 -0500

    remove reference to prod.secret.exs

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,16 @@ use Mix.Config
 # which you typically run after static files are built.
 config :kawaii_kwotes, KawaiiKwotes.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [host: "kawaii-kwotes.herokuapp.com", port: 80],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+
+config :hello_phoenix, HelloPhoenix.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -62,6 +70,5 @@ config :microsoftbot,
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-
-
-import_config "prod.secret.exs"
+# this doesn't work on heroku
+# import_config "prod.secret.exs"
