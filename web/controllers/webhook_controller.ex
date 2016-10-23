6bb74db51a947b43f043d5b837aa9d55aa846739
Souvik Banerjee <souvik1997@gmail.com>
2016-10-22 19:54:13 -0500

    message sending

--- web/controllers/webhook_controller.ex
+++ web/controllers/webhook_controller.ex
@@ -7,4 +7,38 @@ defmodule KawaiiKwotes.WebHookController do
     sender = FacebookMessenger.Response.message_senders(msg) |> hd
     FacebookMessenger.Sender.send(sender, text)
   end
+
+  def manager do
+    Application.get_env(:facebook_messenger, :request_manager) || FacebookMessenger.RequestManager
+  end
+
+  @spec send(String.t, String.t) :: HTTPotion.Response.t
+  def send(recepient, message) do
+    res = manager.post(
+      url: url,
+      body: json_payload(recepient, message)
+    )
+    Logger.info("response from FB #{inspect(res)}")
+    res
+  end
+
+  def json_payload(recepient, message) do
+    payload(recepient, message)
+    |> Poison.encode
+    |> elem(1)
+  end
+
+  def payload(recepient, message) do
+    %{message | :recipient => %{id: recepient}}
+  end
+
+
+  def url do
+    query = "access_token=#{page_token}"
+    "https://graph.facebook.com/v2.6/me/messages?#{query}"
+  end
+
+  def page_token do
+    Application.get_env(:facebook_messenger, :facebook_page_token)
+  end
 end
