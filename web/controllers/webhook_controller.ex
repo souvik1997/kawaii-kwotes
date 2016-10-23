f9421464c139c37bad5b1e4a20bb87819d41faa9
Souvik Banerjee <souvik1997@gmail.com>
2016-10-22 21:38:49 -0500

    sender

--- web/controllers/webhook_controller.ex
+++ web/controllers/webhook_controller.ex
@@ -5,6 +5,7 @@ defmodule KawaiiKwotes.WebHookController do
 
   def message_received(msg) do
     text = FacebookMessenger.Response.message_texts(msg) |> hd
+    sender = FacebookMessenger.Response.message_senders(msg) |> hd
     Logger.info("received text: #{inspect(text)}")
     case String.contains?(text, ["#"]) do
       true ->
