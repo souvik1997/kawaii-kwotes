559ce0bbb1574df89d34b2430fcd8dc56441b379
Souvik Banerjee <souvik1997@gmail.com>
2016-10-22 21:42:46 -0500

    reset

--- web/controllers/webhook_controller.ex
+++ web/controllers/webhook_controller.ex
@@ -7,21 +7,6 @@ defmodule KawaiiKwotes.WebHookController do
     text = FacebookMessenger.Response.message_texts(msg) |> hd
     sender = FacebookMessenger.Response.message_senders(msg) |> hd
     Logger.info("received text: #{inspect(text)}")
-    case String.contains?(text, ["#"]) do
-      true ->
-        Logger.info("error message from FB: #{text}")
-      false ->
-        sender = FacebookMessenger.Response.message_senders(msg) |> hd
-        # sendResponse(sender, %{text: text })
-        # used to be FacebookMessenger.Sender.send(sender, text)
-        _ = FacebookMessenger.Sender.send(sender, %{message: %{attachment: %{
-                                                                  type: "image",
-                                                                  payload: %{
-                                                                    url: "http://images6.fanpop.com/image/photos/34600000/Kawaii-girl-kawaii-anime-34624723-392-500.jpg"
-                                                                  }
-                                                               }}})
-        _ = FacebookMessenger.Sender.send(sender, %{message: %{text: "kawaii"}})
-        :ok
-    end
+    FacebookMessenger.Sender.send(sender, %{message: %{text: text}})
   end
 end
